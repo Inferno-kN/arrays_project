84d7d9a6fa5f8535d762d987e16078025c4064a2
Pasha <aldosovich@bk.ru>
2024-12-16 10:28:42 +0300

    Добавлена функция mean_1d для одномерного массива

--- array_lib.py
+++ array_lib.py
@@ -9,3 +9,19 @@ def prod_1d(arr):
     for i in arr:
         multi *= i
     return multi
+
+def mean_1d(arr):
+    average = sum_1d(arr) / arr(len(arr))
+    return average
+
+def max_1d(arr):
+    max1 = arr[0]
+    if max1 < arr[1]:
+        max1 = arr[1]
+    return max1
+
+def min_1d(arr):
+    min1 = arr[0]
+    if min1 > arr[1]:
+        min1 = arr[1]
+    return min1
